7dddf5364ecb1df5aef458aa2a50681e0265616b
artmende <artmende@student.s19.be>
2022-05-23 17:41:38 +0200

    module08 ex00 done

--- module_08/ex00/easyfind.hpp
+++ module_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: artmende <artmende@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 15:17:33 by artmende          #+#    #+#             */
-/*   Updated: 2022/05/20 17:19:26 by artmende         ###   ########.fr       */
+/*   Updated: 2022/05/23 17:35:01 by artmende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,10 @@
 # include <iostream>
 
 template<typename T>
-int	easyfind(T container, int n)
+typename T::iterator	easyfind(T & container, int n)
 {
-	for (typename T::const_iterator i = container.begin(); i != container.end(); i++)
-	{
-		if (*i == n)
-		{
-			std::cout << "Found it !" << std::endl;
-			return (*i);
-		}
-	}
-	std::cout << "Didnt find it..." << std::endl;
-	return (-1);
+	typename T::iterator	i = std::find(container.begin(), container.end(), n);
+	return (i);
 }
-
 
 #endif
--- module_08/ex00/main.cpp
+++ module_08/ex00/main.cpp
@@ -6,34 +6,49 @@
 /*   By: artmende <artmende@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 15:17:10 by artmende          #+#    #+#             */
-/*   Updated: 2022/05/23 09:30:58 by artmende         ###   ########.fr       */
+/*   Updated: 2022/05/23 17:40:36 by artmende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <iostream>
 #include <vector>
+#include <list>
 #include <algorithm>
 
 int	main()
 {
-	std::vector<int> b = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	std::vector<int> v;
+	std::list<int> l;
 
-//	easyfind(b, 5);
-
-
-	std::vector<int>::iterator	np = std::find(b.begin(), b.end(), -6);
-
-//	int	*nb = std::find(b.begin(), b.end(), 2);
-
-	while (np != b.end())
+	for (int i = 0; i < 10; i++)
 	{
-		std::cout << *np << std::endl;
-		++np;
+		v.push_back(i);
+		l.push_back(i);
 	}
-	std::cout << *np << std::endl;
 
-//	std::cout << *(++np) << std::endl;
+	std::cout << "Content of the vector :" << std::endl;
+	for (std::vector<int>::iterator i = v.begin(); i != v.end(); i++)
+	{
+		std::cout << *i << std::endl;
+	}
 
+	std::cout << "Content of the list :" << std::endl;
+	for (std::list<int>::iterator i = l.begin(); i != l.end(); i++)
+	{
+		std::cout << *i << std::endl;
+	}
 
+	std::cout << "Showing elements in vector from the value 5" << std::endl;
+	for (std::vector<int>::iterator i = easyfind<std::vector<int> >(v, 5); i != v.end(); i++)
+	{
+		std::cout << *i << std::endl;
+	}
+	
+	std::cout << "Showing elements in list from the value 5" << std::endl;
+	for (std::list<int>::iterator i = easyfind<std::list<int> >(l, 5); i != l.end(); i++)
+	{
+		std::cout << *i << std::endl;
+	}
 	return 0;
 }
