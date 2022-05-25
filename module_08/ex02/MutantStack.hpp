0fb2490593aaf73147051a612af2ac3075c1a48f
artmende <artmende@student.s19.be>
2022-05-25 11:29:09 +0200

    ex02 module08 functionnal. need to create the main for test

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: artmende <artmende@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 16:02:25 by artmende          #+#    #+#             */
-/*   Updated: 2022/05/24 10:05:06 by artmende         ###   ########.fr       */
+/*   Updated: 2022/05/25 11:28:11 by artmende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,36 @@
 
 # include <iostream>
 # include <stack>
+# include <iterator>
 
 template <typename T>
 class MutantStack : public std::stack<T>
 {
-private:
-	/* data */
 public:
-	MutantStack();
-	MutantStack(MutantStack const & src);
-	virtual ~MutantStack();
+	typedef typename std::stack<T>::container_type::iterator				iterator;
+	typedef typename std::stack<T>::container_type::const_iterator			const_iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator		reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
 
-	MutantStack &	operator=(MutantStack const & rhs);
+	iterator	begin()
+	{
+		return (this->c.begin());
+	}
+
+	reverse_iterator	rbegin()
+	{
+		return (this->c.rbegin());
+	}
+
+	iterator	end()
+	{
+		return (this->c.end());
+	}
+
+	reverse_iterator	rend()
+	{
+		return (this->c.rend());
+	}
 };
 
 #endif
---
+++ module_08/ex02/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artmende <artmende@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/25 11:17:32 by artmende          #+#    #+#             */
+/*   Updated: 2022/05/25 11:26:52 by artmende         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "MutantStack.hpp"
+
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		(*it)+=5;
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
